3821e45c17a9e58067bba2da434bac4020752471
Bishakh Ghosh <ghoshbishakh@gmail.com>
2016-05-13 10:30:28 +0530

    Configure python-social-auth to associate accounts with same email id

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse('''Hello, world. Welcome to dipy website.''', 200)
