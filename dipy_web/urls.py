a9741f07761ba5780afaf25e59a57c3751028476
Bishakh Ghosh <ghoshbishakh@gmail.com>
2016-07-22 12:34:43 +0530

    Add simple visualization using D3

--- dipy_web/urls.py
+++ dipy_web/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('website.urls')),
+    url(r'^', include('github_visualization.urls')),
 ]
 
 handler404 = 'website.views.custom404'
---
+++ github_visualization/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GithubVisualizationConfig(AppConfig):
+    name = 'github_visualization'
