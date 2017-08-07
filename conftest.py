ba573777d93320f3b44dedc2603ed7ea45507323
skoudoro <skoudoro@gmail.com>
2017-08-07 15:20:10 -0400

    create settings for travis

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import django
 from django.conf import settings
 # We manually designate which settings we will be using in an environment variable
 # This is similar to what occurs in the `manage.py`
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dipy_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dipy_web.test_settings')
 
 # `pytest` automatically calls this function once when tests are run.
 def pytest_configure():
