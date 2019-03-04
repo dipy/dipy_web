8f79a5dc3cc137931e38bf51ae9e50abb70fc75d
Serge Koudoro <skab12@gmail.com>
2019-03-05 00:55:02 +0100

    Add docs for 0.15.0.dev

---
+++ 0.15.0.dev/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.15.0dev',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.fjson',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
