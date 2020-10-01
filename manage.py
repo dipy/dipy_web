5c9623a5516f118b4ec1e7c46df06e90066e7af7
pareekaabhi33 <72213935+pareekaabhi33@users.noreply.github.com>
2020-10-01 21:26:22 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+#import is used to import function 
 import os
 import sys
 
-if __name__ == "__main__":
+if __name__ == "___________main____________":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dipy_web.settings")
 
     from django.core.management import execute_from_command_line
