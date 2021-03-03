29c5350fe15ea5d4f809873fd69b059fd7fa6a26
Serge Koudoro <skab12@gmail.com>
2021-03-03 07:43:56 +0100

    add media to url

--- dipy_web/urls.py
+++ dipy_web/urls.py
@@ -29,9 +29,9 @@ urlpatterns = [
     path('payment/', include('payment.urls', namespace='payment')),
     path('', include('website.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
 
 handler403 = 'website.views.custom403'
 handler404 = 'website.views.custom404'
